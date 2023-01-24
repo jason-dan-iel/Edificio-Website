5c9bb5222924dec87ea156470bc703030049e9c6
Kunal Tiwari <tiwari.25@iitj.ac.in>
2023-01-25 00:42:27 +0530

    added the schema for the core-team member

---
+++ server/models/coreTeam.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const teamSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    instagramLink: String,
+    linkedInLink: String
+
+})
+
+module.exports = teamSchema;
