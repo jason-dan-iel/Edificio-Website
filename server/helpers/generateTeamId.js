4086683ba545c4e87486a01b90dddf59cd014314
Kunal Tiwari <tiwari.25@iitj.ac.in>
2023-01-26 18:10:25 +0530

    wrote the condition to check if the team member is captain or not

--- server/helpers/generateTeamId.js
+++ server/helpers/generateTeamId.js
@@ -1,7 +1,30 @@
-const generateTeamId = () => {
+const User = require('../models/UserModel');
+
+
+// this is a function to get a unique teamId for each  user
+const generateTeamId = (req, res, next) => {
+    const givenUser = req.body
+    
+    // checking if the given user is a captain or not
+    if(givenUser.captain == false){
+        return;
+        next()
+    }
+
+    // generating the teamId
     let teamId = Math.random() * 1000
-    teamId = Math.floor(teamId);
-    return `ED-${teamId}`;
+    teamId = Math.floor(teamId).toString();
+    teamId = `ED-${teamId}`;
+
+    
+    // finding if there is any user with the given TeamId
+    const user = User.findOne({teamId: teamId})
+    if(!user){
+        return teamId;
+        next()
+    }else{
+        generateTeamId();
+    }
 }
 
 
