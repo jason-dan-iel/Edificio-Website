a746f8fdb8b1d762d62fb6c37e5bdafac5263568
Kunal Tiwari <tiwari.25@iitj.ac.in>
2023-01-27 16:02:25 +0530

    added the logic of adding new sponsors

--- server/middleware/handleSponsors.js
+++ server/middleware/handleSponsors.js
@@ -4,3 +4,21 @@ const getSponsors = async (req, res)=>{
     const Sponsor = await SponsorsModel.find()
     res.json(Sponsor);
 }
+
+const addSponsor = async (req, res)=>{
+    const sponsor = req.body;
+    const newSponsor = new SponsorsModel(sponsor);
+
+    try{
+        await newSponsor.save();
+        res.status(201).json(newSponsor);
+    }catch(error){
+        res.status(400).json(error.message);
+    }
+}
+
+
+module.exports = {
+    addSponsor,
+    getSponsors
+}
