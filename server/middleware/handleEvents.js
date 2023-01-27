92f78d76082963f35c882b93dd0c6b3ee979bac3
Kunal Tiwari <tiwari.25@iitj.ac.in>
2023-01-27 16:17:35 +0530

    added the middleware for adding and getting events

---
+++ server/middleware/handleEvents.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const EventsModel = require('../models/eventsModel');
+
+const getEvents = async (req, res)=>{
+    const events = await EventsModel.find()
+    res.json(events);
+}
+
+const addEvent = async(req, res)=>{
+    const event = req.body;
+    const newEvent = new EventsModel(event);
+
+    try{
+        await newEvent.save();
+        res.status(201).json(newEvent); 
+    }catch(error){
+        res.status(400).json(error.message);
+    }
+}
+
+modeule.exports = {
+    getEvents, 
+    addEvent
+}
