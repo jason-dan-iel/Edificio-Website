b9970b97ee043fca34668926d11ca977a8db1db7
Kunal Tiwari <tiwari.25@iitj.ac.in>
2023-03-01 23:18:11 +0530

    added the about us section

--- client/src/components/aboutus.jsx
+++ client/src/components/aboutus.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function AboutUs(){
     return (
-        <main>
-            <h1 className="text-1xl">About Us</h1>
-            <hr />
+        <main className="w-3/5 m-auto text-white">
+            <h1 className="text-2xl font-semibold ">About Us</h1>
+            <hr className="w-3/5 pb-5 mt-5"/>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde quisquam architecto, sit, sint quis accusantium maxime doloremque iste ipsa fugiat neque. Illum at repellendus optio voluptas ex ab earum, asperiores dicta aperiam voluptate recusandae placeat numquam quidem explicabo doloribus dolor?</p>
         </main>
     )
