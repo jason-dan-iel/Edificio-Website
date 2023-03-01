5d5c10b8d8702af76b4fd8690b956c927f8945ff
Kunal Tiwari <tiwari.25@iitj.ac.in>
2023-03-01 23:13:17 +0530

    added homepage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ParticlesConfig from './components/particleConfig';
-
+import { BrowserRouter as Router, Routes, Route}from 'react-router-dom';
+import Home from './pages/home';
 
 
 const App = () => {
   return (
     <div>
       <ParticlesConfig />
+      <Router>
+        <Routes>
+          <Route path = '/' element={<Home />} />
+        </Routes>
+      </Router>
 
     </div>
 
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import AboutUs from "../components/aboutus";
+
+export default function Home(){
+    return (
+        <main>
+            <AboutUs />
+        </main>
+    )
+}
