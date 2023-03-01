7f5b7037c6f8a8fbfc385f70427d1fd3172d0fab
Kunal Tiwari <tiwari.25@iitj.ac.in>
2023-03-01 23:24:22 +0530

    made footer

---
+++ client/src/components/footer.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function Footer(){
+    return (
+        <footer>
+            <ul>
+                <h1>Edificio</h1>
+                <li>Indian Institute of Technology Jodhpur NH 65 Nagaur Road Karwar 342030, Jodhpur District</li>
+                <li>Phone Number: +91 9523218750</li>
+                <li>Email: edificio@iitj.ac.in</li>
+            </ul>
+
+            <ul>
+                <h1>Quick Links</h1>
+                <li>Home</li>
+                <li>Events</li>
+                <li>Core Team</li>
+            </ul>
+
+            <ul>
+                <h1>External Links</h1>
+                <li>IIT Jodhpur</li>
+            </ul>
+
+            <ul>
+                <h1>Socials</h1>
+                <li>Instagram</li>
+                <li>Linkedin</li>
+            </ul>
+        </footer>
+    )
+}
