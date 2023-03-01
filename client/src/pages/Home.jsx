5bbe523ead2950e5f98bc945c3249e72acbae563
Kunal Tiwari <tiwari.25@iitj.ac.in>
2023-03-02 00:31:25 +0530

    done with the navbar

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import AboutUs from "../components/aboutus";
 import EventSection from "../components/eventSection";
 import Footer from "../components/footer";
+import Navbar from "../components/Navbar";
 
 export default function Home(){
     return (
         <main>
+            <Navbar />
             <AboutUs />
             <EventSection />
             <Footer />
