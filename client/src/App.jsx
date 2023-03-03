18ce3ba194babe1379e1135c9e74f7e9439db7ba
Jason Daniel <jason@Jasons-MacBook-Air.local>
2023-03-03 17:52:51 +0530

    merged both the brances, to work on events and eventcard and etc.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,23 +4,25 @@ import { BrowserRouter as Router, Routes, Route}from 'react-router-dom';
 import Home from './pages/home';
 import OurTeam from './pages/OurTeam';
 import Register from './pages/OurTeam copy';
-import Login from './pages/Login';
+import LoginPage from './pages/Login';
 import Navbar from './components/Navbar';
 import Partbg from './components/particles';
+import SignupPage from './pages/Signup';
+import Events from './pages/Events';
 
 const App = () => {
   return (
-    <div>
+    <div >
       <ParticlesConfig />
       {/* <Partbg/> */}
       <Router>
       <Navbar />
         <Routes>
           <Route path = '/' element={<Home />} />
-          <Route path = '/events' element={< Event/>} />
+          <Route path = '/events' element={< Events/>} />
           <Route path = '/ourteam' element={<OurTeam/>} />
-          <Route path = '/login' element={<Login/>} />
-          <Route path = '/register' element={<Register/>} />
+          <Route path = '/login' element={<LoginPage/>} />
+          <Route path = '/signup' element={<SignupPage/>} />
         </Routes>
       </Router>
 
