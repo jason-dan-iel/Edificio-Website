bf37a90fbb40faebe9fc59efa90646c502e0b380
Kunal Tiwari <tiwari.25@iitj.ac.in>
2023-03-05 11:05:31 +0530

    done with the hero section

--- client/src/components/hero.jsx
+++ client/src/components/hero.jsx
@@ -1,9 +1,39 @@
 import React from "react";
+import { TagCloud } from "@frank-mayer/react-tag-cloud";
 
-export default function Hero(){
-    return(
-        <main className="w-screen h-screen">
-            
+
+export default function Hero() {
+    return (
+        <main className="w-screen h-screen flex text-white bg-black">
+            <section className="basis-1/2 flex flex-col justify-center items-center">
+                <h1 className="text-7xl">Edificio 23</h1>
+                <p>Build Innovate Connec</p>
+            </section>
+
+            <section className="basis-1/2 text-white flex justify-center items-center">
+                <TagCloud
+                    options={(w) => ({
+                        radius: Math.min(500, w.innerWidth, w.innerHeight) / 2,
+                        maxSpeed: "fast",
+                    })}
+                    onClick={(tag, ev) => alert(tag)}
+                    onClickOptions={{ passive: true }}
+                >
+                    {[
+                        "VSCode",
+                        "TypeScript",
+                        "React",
+                        "Preact",
+                        "Parcel",
+                        "Jest",
+                        "Next",
+                        "ESLint",
+                        "Framer Motion",
+                        "Three.js",
+                    ]}
+                </TagCloud>
+            </section>
+
         </main>
     )
 }
