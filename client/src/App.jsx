405b17d73b8f816b6d81c218e368b61721dc9039
Jason Daniel <jason@Jasons-Air.Excitel>
2023-03-06 15:10:54 +0530

    frontend deploy

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ParticlesConfig from "./components/particleConfig";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import OurTeam from "./pages/OurTeam";
 import LoginPage from "./pages/Login";
 import Navbar from "./components/Navbar";
