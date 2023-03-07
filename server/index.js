0c4927376a7db2bc77b9f726800295ca47c1da42
Jason Daniel <jason@Jasons-Air.Excitel>
2023-03-07 20:57:19 +0530

    fixed cors issues

--- server/index.js
+++ server/index.js
@@ -9,12 +9,7 @@ const authRoute = require('./routes/auth');
 
 // Middleware 
 app.use(express.json());
-const corsOptions ={
-    origin:'http://localhost:5173', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+app.use(cors());
 
 // Route middleware
 app.use('/api/user', authRoute);
