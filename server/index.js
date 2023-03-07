6339e12146b172bc71f3bc88508e55ac48bbef33
Jason Daniel <jason@Jasons-Air.Excitel>
2023-03-07 19:32:22 +0530

    json object edited

--- server/index.js
+++ server/index.js
@@ -9,7 +9,12 @@ const authRoute = require('./routes/auth');
 
 // Middleware 
 app.use(express.json());
-app.use(cors());
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 // Route middleware
 app.use('/api/user', authRoute);
