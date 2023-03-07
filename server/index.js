1c337e95316b339bc5e43f1e0a1d822f49e99b31
Jason Daniel <jason@Jasons-Air.Excitel>
2023-03-07 21:08:35 +0530

    fixed cors issues

--- server/index.js
+++ server/index.js
@@ -9,7 +9,13 @@ const authRoute = require('./routes/auth');
 
 // Middleware 
 app.use(express.json());
-app.use(cors());
+const Options = {
+    origin: 'https://www.edificioiitj.org.in/',
+  optionsSuccessStatus: 200, 
+//   Access-Control-Allow-Origin: *
+
+}
+app.use(cors(Options));
 
 // Route middleware
 app.use('/api/user', authRoute);
